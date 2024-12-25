045e540b31c50fa10edb6b73141c3a90c60618cb
Michal <michal.drabik0@gmail.com>
2024-12-25 12:51:19 +0100

    Refactor

--- app/src/main/java/com/compose/showcase/core/home/views/MenuButton.kt
+++ app/src/main/java/com/compose/showcase/core/home/views/MenuButton.kt
@@ -1,8 +1,10 @@
 package com.compose.showcase.core.home.views
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -14,16 +16,20 @@ import com.compose.showcase.ui.theme.ShowcaseTheme
 
 @Composable
 internal fun MenuButton(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.image_menu),
+    Icon(
+        painter = painterResource(R.drawable.ic_menu),
         contentDescription = null,
+        tint = ShowcaseTheme.colors.topMenuButtonIcon,
         modifier = modifier
             .shadowMedium(
                 shape = CircleShape,
                 color = ShowcaseTheme.colors.shadowPrimary,
             )
             .size(ShowcaseTheme.size.topMenuButton)
-            .clip(CircleShape),
+            .clip(CircleShape)
+            .background(ShowcaseTheme.colors.topMenuButtonBackground)
+            .wrapContentSize()
+            .size(ShowcaseTheme.size.topMenuButtonIcon),
     )
 }
 
